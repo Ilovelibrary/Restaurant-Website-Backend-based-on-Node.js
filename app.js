d369609092b1d9af0f5a5639bcc6c09eb3c17630
Pu <p5gao@ucsd.edu>
2018-01-12 13:33:03 -0800

    passport

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ mongoose.Promise = require('bluebird');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const Dishes = require('./models/dishes');
 // Connection URL
 const url = 'mongodb://localhost:27017/conFusion';
@@ -50,26 +53,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
 function auth (req, res, next) {
     console.log(req.session);
 
-  if(!req.session.user) {
+  if(!req.user) {
       var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+      res.setHeader('WWW-Authenticate', 'Basic');                          
+      err.status = 401;
+      next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
       next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
   }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,16 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
     admin:   {
         type: Boolean,
         default: false
@@ -18,5 +10,7 @@ var User = new Schema({
 }, {
     usePushEach: true
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
