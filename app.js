cfe0204676068b1ec726d4fe44e6ca77cdba21ff
Pu <p5gao@ucsd.edu>
2018-01-21 19:52:38 -0800

    File Upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -74,6 +75,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
